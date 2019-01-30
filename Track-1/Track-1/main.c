61b6185c1b13f93c7b43c94d22675ec65a2363e6
Patrick <Patrick.dejong@protonmail.com>
2019-01-30 10:22:41 +0100

    [Added] Startup Project for week 1

---
+++ Track-1/Track-1/main.c
@@ -0,0 +1,19 @@
+/*
+ * Track-1.c
+ *
+ * Created: 30/01/2019 10:20:55
+ * Author : Patrick, Simon, Tom
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		printf("Hello World");
+    }
+}
+
