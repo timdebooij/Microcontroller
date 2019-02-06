bd02705a40be65d51419e90ddd25c3dd1ad8e7bb
Unknown <timdebooij9@hotmail.com>
2019-02-06 12:56:40 +0100

    [Added] Comments to Track-2 code

--- Track-2/Track-2/b2.c
+++ Track-2/Track-2/b2.c
@@ -9,32 +9,18 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/iom128a.h>
 
 #include "util.h"
 
+//The position determines which light is activated on PORTD
 int position = 3;
 
-void setLightNext(){
-	
-	
-}
-
-ISR( INT1_vect ){
+/*
+This function turns off the current activated light and activates the next one 
+*/
+void setLight(){
 	position+=1;
-	if(position == 8){
-		position = 3;
-	}
-	int oldPos = position-1;
-	if(oldPos == -1){
-		oldPos = 7;
-	}
-	PORTD &= ~(1<<oldPos);
-	wait(200);
-	PORTD |= (1<<position);
-}
-
-ISR( INT2_vect){
-	position += 1;
 	if(position == 8){
 		position = 3;
 	}
@@ -43,12 +29,29 @@ ISR( INT2_vect){
 		oldPos = 7;
 	}
 	PORTD &= ~(1<<oldPos);
-
+	wait(100);
 	PORTD |= (1<<position);
+}
+
+ISR(INT0_vect){
+	
+}
+
+//Is called when INT1 is pressed
+ISR( INT1_vect ){
+	setLight();
+}
+
+//Is called when INT2 is pressed
+ISR( INT2_vect ){
+	setLight();
 }
 
 
 
+/*
+In this function the last five bits of Direction D are set to output and the INT0, INT1 and INT2 are activated.
+*/
 void runb2(){
 	DDRD = 0b11111000;
 	EIMSK |= 0x07;
