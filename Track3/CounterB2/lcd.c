b66fe3fa6ead7cbc204658ed2dee74910faa164c
Unknown <ketsh11@hotmail.com>
2019-02-13 17:59:04 +0100

    [Added] B2

--- Track3/CounterB2/lcd.c
+++ Track3/CounterB2/lcd.c
@@ -6,11 +6,6 @@
 #define LCD_E 	3
 #define LCD_RS	2
 
-void lcd_strobe_lcd_e(void);
-void init_4bits_mode(void);
-void lcd_write_string(char *str);
-void lcd_write_data(unsigned char byte);
-void lcd_write_cmd(unsigned char byte);
 
 /******************************************************************/
 void lcd_strobe_lcd_e(void)
@@ -26,6 +21,11 @@ Version :    	DMK, Initial code
 	_delay_ms(1);			// nodig
 	PORTC &= ~(1<<LCD_E);  	// E low
 	_delay_ms(1);			// nodig?
+}
+
+void lcd_clear()
+{
+	lcd_write_command(0x01);
 }
 
 /******************************************************************/
---
+++ Track3/CounterB2/main.c
@@ -0,0 +1,46 @@
+/*
+ * CounterB2.c
+ *
+ * Created: 13-2-2019 14:51:18
+ * Author : Tom Martens
+ */ 
+
+#define F_CPU 8000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
+
+#define BIT(x)(1 << (x))
+
+void wait (int ms);
+
+unsigned int i = 0; 
+
+int main( void )
+{
+	TCCR2 = 0x07;
+	unsigned char *c[7];
+	DDRD &= ~BIT(7);
+	DDRA = 0x00;
+	init_4bits_mode();
+	while (1)
+	{
+		if((PINA & 1) > 0)
+		{
+			i += 1;
+			
+			lcd_clear();
+			itoa(i, c, 10);
+			lcd_write_string(c);
+		}
+		wait(200);
+	}
+}
+
+void wait (int ms)
+{
+	for (int i  = 0; i<ms; i++){
+		_delay_ms(1);
+	}
+}
